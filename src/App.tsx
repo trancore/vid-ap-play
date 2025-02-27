53f022544eed641bf9d7be2d55606ab9e15ee77c
trancore <ki.program0915@gmail.com>
2025-02-27 23:51:42 +0900

    feature: add tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { invoke } from '@tauri-apps/api/core'
 import './App.css'
+import './assets/global.css'
 
 function App() {
   const [greetMsg, setGreetMsg] = useState('')
