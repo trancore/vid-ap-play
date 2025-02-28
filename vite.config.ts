db9c94a5a4262014c6e3dff2237008f54504f3c3
trancore <ki.program0915@gmail.com>
2025-03-01 00:40:06 +0900

    feature: remove comment

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ const host = process.env.TAURI_DEV_HOST
 export default defineConfig(async () => ({
   plugins: [react(), tailwindcss()],
   resolve: {
-    // ↓ 追記
     alias: {
       '~/': path.join(__dirname, 'src/'),
     },
