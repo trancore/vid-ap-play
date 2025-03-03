ea934e8eeb9e9ddea95330ec1f3f165ef5bc8b94
trancore <ki.program0915@gmail.com>
2025-03-03 22:55:42 +0900

    feature: add Layout component

---
+++ src/components/Common/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+﻿import { ReactNode } from 'react'
+import Header from '~/components/Common/Header/Header'
+
+interface Props {
+  children: ReactNode
+}
+
+export default function Layout({ children }: Props): JSX.Element {
+  return (
+    <>
+      <Header />
+      <main className="px-8 py-4">{children}</main>
+    </>
+  )
+}
