88366c01b0ce4533c3e52747743d9041949e9f09
trancore <ki.program0915@gmail.com>
2025-03-03 22:52:34 +0900

    feature: add VideoCard component

---
+++ src/components/Ui/Card/VideoCard.tsx
@@ -0,0 +1,40 @@
+﻿import { Button } from '@headlessui/react'
+import { useState } from 'react'
+import { formatDuration } from '~/utils/format'
+import VideoDialog from '../Dialog/VideoDialog'
+
+interface Props {
+  src: string
+  fileName: string
+  duration: number
+}
+
+export default function VideoCard({
+  src,
+  fileName,
+  duration,
+}: Props): JSX.Element {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function open() {
+    setIsOpen(true)
+  }
+
+  return (
+    <>
+      <div className="max-w-80">
+        <Button
+          className="flex w-full flex-col items-start gap-1 hover:cursor-pointer"
+          onClick={open}
+        >
+          <video src={src} />
+          <p className="w-full overflow-hidden text-left text-ellipsis">
+            {fileName}
+          </p>
+          <p>{formatDuration(duration)}</p>
+        </Button>
+      </div>
+      <VideoDialog src={src} isOpen={isOpen} setIsOpen={setIsOpen} />
+    </>
+  )
+}
