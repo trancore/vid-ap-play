9b4276e8925b9d4c3986fde481183786b8df24af
trancore <ki.program0915@gmail.com>
2025-03-03 22:53:50 +0900

    feature: add Header component

---
+++ src/components/Common/Header/Header.tsx
@@ -0,0 +1,10 @@
+﻿export default function Header(): JSX.Element {
+  return (
+    <header className="sticky top-0 flex px-4 py-8">
+      <div className="flex">
+        <img src="/vid-ap-play.svg" className="size-8"></img>
+        <h1 className="text-3xl">VidApPlay</h1>
+      </div>
+    </header>
+  )
+}
