090a6bd8238975921982232558eb3f5f9170afdf
trancore <ki.program0915@gmail.com>
2025-03-06 22:48:17 +0900

    feature: add set method and get tab method in useTab

---
+++ src/hooks/useTab.ts
@@ -0,0 +1,28 @@
+﻿import { useState } from 'react'
+
+interface Tab {
+  id: number
+  path: string
+  active: boolean
+}
+
+export default function useTab() {
+  const [tabs, setTabs] = useState<Tab[]>()
+
+  function getTab(id: number): Tab | undefined {
+    return tabs?.find((tab) => tab.id === id)
+  }
+
+  function setTab(tab: Tab) {
+    if (tabs === undefined) return
+    const currentTabs = structuredClone(tabs)
+    currentTabs.push({
+      id: currentTabs.length + 1,
+      path: tab.path,
+      active: tab.active,
+    })
+    setTabs(currentTabs)
+  }
+
+  return { tabs, setTabs, getTab, setTab }
+}
