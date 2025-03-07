7998ef29217df10549d0c03e4db78d862f4863e0
trancore <ki.program0915@gmail.com>
2025-03-08 00:22:38 +0900

    feature: display tabs in useTab

--- src/components/Ui/Tab/ToggleTab.tsx
+++ src/components/Ui/Tab/ToggleTab.tsx
@@ -1,27 +1,31 @@
 ﻿import { Tab, TabGroup, TabList, TabPanels } from '@headlessui/react'
 import { ReactNode } from 'react'
+import useTab from '~/hooks/useTab'
 
 interface Props {
   children: ReactNode
 }
 
 export default function ToggleTab({ children }: Props): JSX.Element {
-  const dummy = Array.from({ length: 3 }, (_, i) => i)
+  const { tabs: currentTabs } = useTab()
 
   return (
     <TabGroup>
       <TabList className="sticky top-[116px] flex overflow-x-scroll pb-10">
-        {dummy.map((dummy, index) => (
-          <Tab
-            key={`tab-${index}`}
-            className="w-60 cursor-pointer overflow-hidden border border-gray-500 p-4 text-ellipsis text-orange-700"
-          >
-            {dummy}
+        {currentTabs === undefined || currentTabs.length < 1 ? (
+          <Tab className="min-w-60 cursor-pointer border-b border-gray-500 p-4 text-ellipsis">
+            +
           </Tab>
-        ))}
-        <Tab className="min-w-60 cursor-pointer border-b border-gray-500 p-4 text-ellipsis">
-          +
-        </Tab>
+        ) : (
+          currentTabs.map((tab, index) => (
+            <Tab
+              key={`tab-${index}`}
+              className="w-60 cursor-pointer overflow-hidden border border-gray-500 p-4 text-ellipsis text-orange-700"
+            >
+              {tab.path}
+            </Tab>
+          ))
+        )}
       </TabList>
       <TabPanels>{children}</TabPanels>
     </TabGroup>
