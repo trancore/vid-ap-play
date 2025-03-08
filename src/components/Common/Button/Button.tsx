5dc1ab451c5b1250ee4e64f1a2250257e9cffb04
trancore <ki.program0915@gmail.com>
2025-03-08 16:09:24 +0900

    feature: change FormFile component to Button component

---
+++ src/components/Common/Button/Button.tsx
@@ -0,0 +1,17 @@
+﻿import { Button as HeaadlessButton } from '@headlessui/react'
+
+interface Props {
+  text: string
+  onClick: () => void
+}
+
+export default function Button({ text, onClick }: Props): JSX.Element {
+  return (
+    <HeaadlessButton
+      className="w-fit rounded-lg bg-gray-200 px-3 py-1.5 text-black hover:cursor-pointer"
+      onClick={onClick}
+    >
+      {text}
+    </HeaadlessButton>
+  )
+}
