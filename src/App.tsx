62d09edff671a33760485e3b7d6165d333497262
trancore <ki.program0915@gmail.com>
2025-03-08 17:27:04 +0900

    feature: add plus tag feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,29 @@ import './assets/css/global.css'
 import Layout from '~/components/Common/Layout/Layout'
 import ContentTab from '~/components/Ui/Tab/ContentTab'
 import ToggleTab from '~/components/Ui/Tab/ToggleTab'
+import useDirectory from '~/hooks/useDirectory'
+import useTab from '~/hooks/useTab'
 
 function App() {
+  const { tabs, addTab } = useTab()
+  const { directoryPath, selectDirectory } = useDirectory()
+
   return (
     <Layout>
-      <ToggleTab>
-        <ContentTab></ContentTab>
+      <ToggleTab
+        tabs={tabs}
+        onClickPlusTab={() =>
+          addTab({
+            id: tabs.length + 1,
+            path: directoryPath,
+            active: true,
+          })
+        }
+      >
+        <ContentTab
+          directoryPath={directoryPath}
+          selectDirectory={selectDirectory}
+        />
       </ToggleTab>
     </Layout>
   )
