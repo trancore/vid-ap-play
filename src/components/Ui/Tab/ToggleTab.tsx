8bda7d4cdc1dd090ef1231a308e34e6449ad07b6
trancore <ki.program0915@gmail.com>
2025-03-08 16:49:29 +0900

    feature: update tab +

--- src/components/Ui/Tab/ToggleTab.tsx
+++ src/components/Ui/Tab/ToggleTab.tsx
@@ -12,20 +12,19 @@ export default function ToggleTab({ children }: Props): JSX.Element {
   return (
     <TabGroup>
       <TabList className="sticky top-[116px] flex overflow-x-scroll pb-10">
-        {currentTabs === undefined || currentTabs.length < 1 ? (
-          <Tab className="min-w-60 cursor-pointer border-b border-gray-500 p-4 text-ellipsis">
-            +
-          </Tab>
-        ) : (
-          currentTabs.map((tab, index) => (
-            <Tab
-              key={`tab-${index}`}
-              className="w-60 cursor-pointer overflow-hidden border border-gray-500 p-4 text-ellipsis text-orange-700"
-            >
-              {tab.path}
-            </Tab>
-          ))
-        )}
+        {currentTabs === undefined ||
+          (currentTabs.length > 0 &&
+            currentTabs.map((tab, index) => (
+              <Tab
+                key={`tab-${index}`}
+                className="w-60 cursor-pointer overflow-hidden border border-gray-500 p-4 text-ellipsis text-orange-700"
+              >
+                {tab.path}
+              </Tab>
+            )))}
+        <Tab className="min-w-60 cursor-pointer border-b border-gray-500 p-4 text-ellipsis">
+          +
+        </Tab>
       </TabList>
       <TabPanels>{children}</TabPanels>
     </TabGroup>
