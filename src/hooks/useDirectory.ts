70dc2b1fafd3acfd1a9cce110aa24e9b6457e089
trancore <ki.program0915@gmail.com>
2025-03-08 15:53:37 +0900

    feature: add useDirectory

---
+++ src/hooks/useDirectory.ts
@@ -0,0 +1,19 @@
+﻿import { open } from '@tauri-apps/plugin-dialog'
+import { useState } from 'react'
+
+export default function useDirectory() {
+  const [directoryPath, setDirectoryPath] = useState('')
+
+  const selectDirectory = async () => {
+    const selectedDirectoryPath = await open({
+      directory: true, // ディレクトリ選択モード
+      multiple: false, // 複数選択を無効
+    })
+
+    if (selectedDirectoryPath) {
+      setDirectoryPath(selectedDirectoryPath)
+    }
+  }
+
+  return { directoryPath, selectDirectory }
+}
