829ac8c2ff65d28195a4c3c3966b8e43eac60804
trancore <ki.program0915@gmail.com>
2025-03-09 22:12:10 +0900

    feature: feature video src

--- src/components/Ui/Tab/ContentTab.tsx
+++ src/components/Ui/Tab/ContentTab.tsx
@@ -1,33 +1,55 @@
-﻿import VideoCard from '~/components/Ui/Card/VideoCard'
+﻿import { convertFileSrc } from '@tauri-apps/api/core'
+import { readDir } from '@tauri-apps/plugin-fs'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import VideoCard from '~/components/Ui/Card/VideoCard'
 
+interface IFile {
+  src: string
+  fileName: string
+  duration: number
+}
 interface Props {
   directoryPath: string
 }
 
 export default function ContentTab({ directoryPath }: Props): JSX.Element {
-  const dummy = Array.from({ length: 4 }, (_, i) => {
-    return {
-      src: '/src/assets/videos/dummy-video.mp4',
-      fileName: `${i}-filenamefilenamefilenamefilenamefilenamefilenamefilename`,
-      duration: 3600,
-    }
-  })
+  const [files, setFiles] = useState<IFile[]>([])
+
+  const fetchVideoFiles = useCallback(async () => {
+    const readFiles = await readDir(directoryPath)
+    const currentFiles = readFiles.map<IFile>((file) => ({
+      src: convertFileSrc(directoryPath + '/' + file.name),
+      fileName: file.name,
+      duration: 6000,
+    }))
+
+    setFiles(currentFiles)
+  }, [directoryPath])
+
+  const videoCards = useMemo(() => {
+    return files.map((file, index) => (
+      <VideoCard
+        key={`video-card-${index}`}
+        src={file.src}
+        fileName={file.fileName}
+        duration={file.duration}
+      />
+    ))
+  }, [files])
+
+  useEffect(() => {
+    if (directoryPath === '') return
+    fetchVideoFiles()
+  }, [directoryPath, fetchVideoFiles])
 
   return (
     <div className="flex flex-col gap-10">
       <p>
         {directoryPath ? directoryPath : 'ディレクトリが指定されていません。'}
       </p>
-      <div className="flex flex-wrap gap-x-3 gap-y-10">
-        {dummy.map((dummy, index) => (
-          <VideoCard
-            key={`video-card-${index}`}
-            src={dummy.src}
-            fileName={dummy.fileName}
-            duration={dummy.duration}
-          />
-        ))}
-      </div>
+      {files.length > 0 && (
+        <div className="flex flex-wrap gap-x-3 gap-y-10">{videoCards}</div>
+      )}
     </div>
   )
 }
