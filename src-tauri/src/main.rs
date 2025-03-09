740c50ae59efb9777a3fe0cb049fad43bfb6e115
trancore <ki.program0915@gmail.com>
2025-03-09 19:59:37 +0900

    feature: add tauri-plugin-fs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,7 @@
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .run(tauri::generate_context!())
