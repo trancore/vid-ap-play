1703e0d2bae034688ce79c4182fef2847274e630
trancore <ki.program0915@gmail.com>
2025-03-23 01:49:32 +0900

    feature: remove sticky

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -1,6 +1,6 @@
 ﻿export default function Header(): JSX.Element {
   return (
-    <header className="sticky top-0 flex bg-gray-500 px-4 py-8">
+    <header className="flex bg-gray-500 px-4 py-8">
       <div className="flex">
         <img src="/vid-ap-play.svg" className="size-8"></img>
         <h1 className="text-3xl">VidApPlay</h1>
